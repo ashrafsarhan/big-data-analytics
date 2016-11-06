e3fd5bf52d757b26eb7634df0b5ad2a9601ed781
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-11-06 14:57:26 +0100

    Adding solutions Loops and list comprehensions part

--- python/labs/lab1/PythonLab1.py
+++ python/labs/lab1/PythonLab1.py
@@ -27,3 +27,39 @@ print(ParrotWords)
 #e
 sentence = ' '.join(ParrotWords)
 print(sentence)
+
+"""
+2. Loops and list comprehensions
+"""
+#a
+for num in range(5,11):  #to iterate between 5 to 10
+ print('The next number in the loop is %d' % (num))
+ 
+#b 
+from random import uniform
+while(uniform(0,1) < 0.9):
+  print('The random number is smaller than 0.9')
+  
+#c  
+names = ['Ludwig','Rosa','Mona','Amadeus']
+for n in names:
+    print('The name %s is nice' % (n))
+    
+#d    
+names = ['Ludwig','Rosa','Mona','Amadeus']
+nLetters = []
+for i, n in enumerate(names):
+    nLetters.append(len(n))
+print(nLetters)
+
+#e
+nLetters = [len(n) for n in names]
+print(nLetters)
+
+#f
+shortLong = ['long' if len(n)>4 else 'short' for n in names]
+print(shortLong)
+
+zipall = zip(names, shortLong)
+for n in zipall:
+    print('The name %s is a %s name' % (n[0], n[1]))
