82b153725ae69f68e1854e75cf57e8a7a300a426
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-12-14 13:34:04 +0100

    Update PySpark min/max temperatures extractor

---
+++ BDA/labs/lab1/spark_mmt_extractor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 17:06:24 2016
+
+@author: ashraf
+"""
+from pyspark import SparkContext
+
+iFile = 'data/temperature-readings.csv'
+oFile = 'data/mm_temperature'
+fromYear = 1950
+toYear = 2014
+
+sc = SparkContext(appName="MaxMinExtractor")
+
+lines = sc.textFile(iFile)
+
+lines = lines.map(lambda a: a.split(";"))
+
+lines = lines.filter(lambda x: int(x[1][0:4]) >= 1950 and int(x[1][0:4]) <= 2014)
+
+temperatures = lines.map(lambda x: (x[1][0:4], (x[0], float(x[3]))))
+
+################## Custom MIN/MAX Function ############################
+
+min = (lambda x, y: x if x[1] < y[1] else y)
+max = (lambda x, y: x if x[1] > y[1] else y)
+
+minTemperatures = temperatures.reduceByKey(min)
+
+maxTemperatures = temperatures.reduceByKey(max)
+
+minMaxTemp = minMaxTemp = minTemperatures.union(maxTemperatures).reduceByKey(lambda x,y: (x[0],x[1],y[0],y[1]))
+
+sortedMinMaxTemp = minMaxTemp.sortBy(ascending=False, keyfunc=lambda a: a[1][3])
+
+sortedMinMaxTempCsv = sortedMinMaxTemp.map(lambda a: '%s,%s,%s,%s,%s' % (a[0], a[1][0], a[1][1], a[1][2], a[1][3]))
+
+sortedMinMaxTempCsv.coalesce(1).saveAsTextFile(oFile)
+
