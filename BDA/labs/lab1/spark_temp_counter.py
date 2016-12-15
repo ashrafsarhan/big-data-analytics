ed249aed4262fe7a6a9570adc27f5641231ce74d
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-12-15 10:17:13 +0100

    Adding TempCounterSparkJob

---
+++ BDA/labs/lab1/spark_temp_counter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 17:06:24 2016
+
+@author: ashraf
+"""
+from pyspark import SparkContext
+
+iFile = 'data/temperature-readings.csv'
+oFile = 'data/over_ten_mth_temp_counts'
+fromYear = 1950
+toYear = 2014
+target_temp = 10
+
+sc = SparkContext(appName="TempCounterSparkJob")
+
+lines = sc.textFile('data/head-temperature-readings.csv')
+
+lines = lines.map(lambda a: a.split(";"))
+
+lines = lines.filter(lambda x: int(x[1][0:4]) >= fromYear and int(x[1][0:4]) <= toYear and float(x[3]) > target_temp)
+
+overTenMthTemp = lines.map(lambda x: (x[1][5:7], 1))
+
+overTenMthTempCounts = overTenMthTemp.reduceByKey(lambda v1,v2: v1 + v2)
+
+overTenMthTempCountsCsv = sortedMinMaxTemp.map(lambda a: '%s,%s' % (a[0], a[1]))
+
+overTenMthTempCountsCsv.coalesce(1).saveAsTextFile(oFile)
+
