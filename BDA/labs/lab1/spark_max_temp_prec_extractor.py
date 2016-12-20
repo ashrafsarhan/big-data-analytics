47e714986aaa2dc707877b27886cd537bff1ff62
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-12-21 00:01:22 +0100

    Update spark_max_temp_prec_extractor script

---
+++ BDA/labs/lab1/spark_max_temp_prec_extractor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 17:06:24 2016
+
+@author: ashraf
+"""
+from pyspark import SparkContext
+
+iFile = 'data/temperature-readings.csv'
+iFile2 = 'data/precipitation-readings.csv'
+oFile = 'data/max_temperature_precipitation'
+
+sc = SparkContext(appName="MaxTempPrecExtractorSparkJob")
+
+######### Max Temperatures #########
+temperatures = sc.textFile(iFile)
+temperatures = temperatures.map(lambda a: a.split(";"))
+temperatures = temperatures.map(lambda x: (x[0], float(x[3])))
+maxTemperatures = temperatures.reduceByKey(max)
+maxTemperatures = maxTemperatures.filter(lambda a: a[1] > 25 and a[1] < 30)
+
+######### Max Precipitations #########
+precipitations = sc.textFile(iFile2)
+precipitations = precipitations.map(lambda a: a.split(";"))
+precipitations = precipitations.map(lambda x: (x[0], float(x[3])))
+maxPrecipitations = precipitations.reduceByKey(max)
+maxPrecipitations = maxPrecipitations.filter(lambda a: a[1] > 100 and a[1] < 200)
+
+######### Merged Max Temperatures/Precipitations #########
+maxTempPrec = maxTemperatures.union(maxPrecipitations).reduceByKey(lambda x,y: (x, y))
+
+#maxTempPrecCsv = maxTempPrec.map(lambda a: '%s,%s,%s' % (a[0], a[1][0], a[1][1]))
+
+maxTempPrec.coalesce(1).saveAsTextFile(oFile)
+
+
+
