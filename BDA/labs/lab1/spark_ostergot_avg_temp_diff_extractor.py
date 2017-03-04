b5d6581e22cb1b76dcbcc591c4d14f1146b32b63
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2017-03-04 23:15:39 +0100

    Updating Q6 lab1

--- BDA/labs/lab1/spark_ostergot_avg_temp_diff_extractor.py
+++ BDA/labs/lab1/spark_ostergot_avg_temp_diff_extractor.py
@@ -10,6 +10,8 @@ from pyspark import SparkContext
 iFile = 'data/stations-Ostergotland.csv'
 iFile2 = 'data/temperature-readings.csv'
 oFile = 'data/OstergotlandAvgMonthlyDiffTemp'
+fromYear = 1960
+toYear = 2014
 
 sc = SparkContext(appName="OstergotlandAvgMonthlyTempDiffSparkJob")
 
@@ -19,29 +21,38 @@ ostergotlandStations = ostergotlandStations.map(lambda line: line.split(";")).ma
 
 isOstergotlandStation = (lambda s: s in ostergotlandStations)
 
-temperatures = sc.textFile(iFile2)
+temperatures = sc.textFile(iFile2).map(lambda line: line.split(";")). \
+filter(lambda x: isOstergotlandStation(int(x[0])) and int(x[1][0:4]) >= fromYear and int(x[1][0:4]) <= toYear)
 
-temperatures = temperatures.map(lambda line: line.split(";")). \
-filter(lambda x: isOstergotlandStation(int(x[0])) and int(x[1][0:4]) >= 1950 and int(x[1][0:4]) <= 2014)
+monthlyAvgTemps = temperatures.map(lambda obs:
+                                             ((obs[1], int(obs[0])),
+                                                    (float(obs[3]), float(obs[3])))) \
+                                              .reduceByKey(lambda (mint1, maxt1), (mint2, maxt2):
+                                                           (min(mint1, mint2), max(maxt1, maxt2))) \
+                                              .map(lambda ((day, station), (mint, maxt)):
+                                                   (day[:7], (mint + maxt, 2))) \
+                                              .reduceByKey(lambda (temp1, count1), (temp2, count2):
+                                                           (temp1 + temp2, count1 + count2)) \
+                                              .map(lambda (month, (temp, count)):
+                                                   (month, temp / float(count)))
 
-daily_temperatures = temperatures.map(lambda x: (x[1], (float(x[3]), float(x[3])))). \
-reduceByKey(lambda t1, t2: (min(t1[0], t2[0]), max(t1[1], t2[1])))
+monthlyLongtermAvgTemps = monthlyAvgTemps.filter(lambda (month, temp):
+                                                    int(month[:4]) <= 1980) \
+                                            .map(lambda (month, temp):
+                                                 (month[-2:], (temp, 1))) \
+                                            .reduceByKey(lambda (temp1, count1), (temp2, count2):
+                                                         (temp1 + temp2, count1 + count2)) \
+                                            .map(lambda (month, (temp, count)):
+                                                 (month, temp / float(count)))
 
-monthly_temperatures = daily_temperatures.map(lambda x:(x[0].split("-")[0]+','+x[0].split("-")[1], (x[1][0]+x[1][1], 2)))
+month_temp = {month: temp for month, temp in monthlyLongtermAvgTemps.collect()}
 
-monthly_temperatures = monthly_temperatures.reduceByKey(lambda v1, v2: (v1[0] + v2[0], v1[1] + v2[1]))
+monthlyAvgTemps = monthlyAvgTemps.map(lambda (month, temp):
+                                        (month, abs(temp) - abs(month_temp[month[-2:]]))) \
+                                   .sortBy(ascending=True, keyfunc=lambda (month, temp): month)
 
-monthly_avg_temperatures = monthly_temperatures.map(lambda x: (x[0], x[1][0] / x[1][1]))
+monthlyAvgTempsCsv = monthlyAvgTemps.map(lambda a: '%s,%s,%s' % (a[0].split('-')[0], a[0].split('-')[1], a[1]))
 
-longterm_monthly_avg_temperatures = monthly_avg_temperatures. \
-filter(lambda x: int(x[0].split(",")[0]) >= 1950 and int(x[0].split(",")[0]) <= 1980). \
-map(lambda x: (x[0].split(",")[1], (x[1], 1))).reduceByKey(lambda t1, t2:(t1[0] + t2[0], t1[1] + t2[1])). \
-map(lambda t: (t[0], t[1][0]/t[1][1]))
+monthlyAvgTempsCsv.repartition(1).saveAsTextFile(oFile)
 
-longTermAvgTempLookup = {month: temp for month, temp in longterm_monthly_avg_temperatures.collect()}
 
-final_res = monthly_avg_temperatures.map(lambda x: (x[0], x[1] - longTermAvgTempLookup[x[0].split(",")[1]]))
-
-monthly_avg_diff_temperatures_csv = final_res.map(lambda a: '%s,%s' % (a[0], a[1]))
-
-monthly_avg_diff_temperatures_csv.coalesce(1).saveAsTextFile(oFile)
